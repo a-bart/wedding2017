dab3cd3c1402cca123c6fc387e4a19463aba69a9
a-bart <alexey.barton@gmail.com>
2017-04-12 20:56:08 +0300

    Initial dist subtree commit

--- Backend/app/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var users = require('../controllers/users');
-
-router.get('/user', users.list);
-router.post('/user', users.post);
-
-module.exports = router;
